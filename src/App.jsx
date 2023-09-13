71ea9052b5b768ac7a53842940ea2e1a0ba6b280
rahuldahra-47 <rahul.dahra59@gmail.com>
2023-09-13 13:48:25 +0530

    Hero Section Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { CustomerReviews, Nav, Footer, SpecialOffers, PopularProducts, Hero, Subscribe, Services, SuperQuality } from "./components"
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className='relative'>
+      <Nav />
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding '>
+        <PopularProducts />
+      </section>
+      <section className='padding '>
+        <SuperQuality />
+      </section>
+      <section className='padding-x py-10 '>
+        <Services />
+      </section>
+      <section className='padding '>
+        <SpecialOffers />
+      </section>
+      <section className='bg-pale-blue padding '>
+        <CustomerReviews />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full '>
+       <Subscribe />
+      </section>
+      <section className='padding-x padding-t pb-8 bg-black '>
+        <Footer />
+      </section>
+    </main>
   )
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Hero from "./Hero"
+import Nav from "./Nav"
+import Services from "./Services"
+import SpecialOffers from "./SpecialOffers"
+import PopularProducts from "./PopularProducts"
+import Footer from "./Footer"
+import Subscribe from "./Subscribe"
+import SuperQuality from "./SuperQuality"
+import CustomerReviews from "./CustomerReviews"
+import Button from "./Button"
+import ShoeCard from "./ShoeCard"
+
+export {
+    Hero,
+    PopularProducts,
+    SpecialOffers,
+    Nav,
+    Services,
+    Footer,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews,
+    Button,
+    ShoeCard
+}
