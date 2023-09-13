a8d4dc1129d484706ac0ab16beee038ab9a8b353
rahuldahra-47 <rahul.dahra59@gmail.com>
2023-09-13 20:59:00 +0530

    Special offer section added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,30 @@
-
-
-const Button = ({label, iconUrl}) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  textColor,
+  borderColor,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red ">
-        {label}
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full     
+      ${
+         backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : " bg-coral-red  text-white border-coral-red "
+} `}
+    >
+      {label}
 
-       {iconUrl && <img src={iconUrl} alt="arrow right" className="ml-2 rounded-full w-5 h-5 " />}
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt="arrow right"
+          className="ml-2 rounded-full w-5 h-5 "
+        />
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- src/components/SpecialOffers.jsx
+++ src/components/SpecialOffers.jsx
@@ -1,7 +1,32 @@
+import { arrowRight } from "../assets/icons"
+import { offer } from "../assets/images"
+import Button from "./Button"
 
 const SpecialOffers = () => {
   return (
-    <div>SpecialOffers</div>
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+          <img src={offer} alt="" width={773} height={687} className="object-contain w-full" />
+      </div>
+      <div className="flex flex-1 flex-col">
+
+<h2 className="mt-10 font-palanquin text-4xl font-bold lg:max-w-lg">
+  <br />
+  <span className="text-coral-red"> Special</span> 
+  <span> Offer</span> 
+</h2>
+<p className="mt-4 lg:max-w-lg info-text">
+Our artisans, with years of experience, handcraft each shoe with precision and care. They pay attention to every detail, from the stitching to the finishing touches, guaranteeing a product that not only looks fantastic but also stands up to the test of time.
+</p>
+<p className="mt-6 lg:max-w-lg info-text">Experience the pinnacle of footwear excellence with our Super Quality Product Shoes</p>
+<div className="mt-11 flex flex-wrap gap-4 ">
+<Button label="Shop now" iconUrl={arrowRight}/>
+<Button label="Learn More" backgroundColor="bg-white" borderColor="border-slate-gray"
+textColor="text-slate-gray" 
+/>
+</div>
+</div>
+    </section>
   )
 }
 
